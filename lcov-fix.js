d43b80acc21a98bd21f63ed6546f739d425bf49c
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-02-16 14:28:51 +0000

    jpex build tools

---
+++ lcov-fix.js
@@ -0,0 +1,16 @@
+var Jpex = require('./jpex');
+var App = Jpex.extend(function($fs, path){
+  var base = path.resolve('.') + path.sep;
+  $fs.readFile(base + 'coverage/lcov.info', 'utf8')
+    .then(function(data){
+      while (data.indexOf(base) > -1){
+        data = data.replace(base, '');
+      }
+      return data.replace(/\\/g, '/');
+    })
+    .then(function(data){
+      return $fs.writeFile(base + 'lcov.info', data, 'utf8');
+    });
+});
+
+App();
